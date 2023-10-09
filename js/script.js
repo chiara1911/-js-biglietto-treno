5bb84f864bd42ea632fad98fec627b474920fc64
chiara1911 <chiaracesari89@gmail.com>
2023-10-09 18:50:15 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
 const km = 0.21;
 
-
-
-
-
 let distanza = parseInt(prompt('quanti km devi fare?'));
 let eta = parseInt(prompt('quanti anni hai?'));
 
@@ -12,20 +8,16 @@ let eta = parseInt(prompt('quanti anni hai?'));
 
 let divBiglietto = document.getElementById('biglietto');
 
-let biglietto = (distanza * km);
+ let ticket = distanza * km;
 
 
+if (eta < 18){
+    ticket = (ticket - (ticket / 100) * 20);
+} else if (eta > 65){
+    ticket =  (ticket - (ticket / 100) * 40);
 
- if (eta < 18){
-    biglietto = 'ticket euro' + '' + (biglietto - ((biglietto / 100) * 20));
-    divBiglietto.innerHTML = biglietto;
-} else if (eta > 66){
-    
-    biglietto = (biglietto- ((biglietto / 100) * 40));
-    divBiglietto.innerHTML = biglietto;
- } else {
-    
-    biglietto = (biglietto.toFixed(2));
-    divBiglietto.innerHTML = biglietto;
- }
+}
+
+ divBiglietto.innerHTML = ' totale euro ' + ticket.toFixed(2);
+
 
